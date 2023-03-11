1fce2e58067cc6eb22b5cf24c1b98b14b9226dea
qudwns4351 <qudwns4351@naver.com>
2023-03-11 22:52:40 +0900

    setting

---
+++ src/main/java/com/side/billim/BillimApplication.java
@@ -0,0 +1,13 @@
+package com.side.billim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BillimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/side/billim/BillimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.side.billim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
