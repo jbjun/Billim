194013c8bcc7057115daa80299de453461cc130a
ssooya <ssooya90@naver.com>
2023-04-09 16:48:27 +0900

    feat swagger init

--- backend/src/main/java/com/side/billim/TestController.java
+++ backend/src/main/java/com/side/billim/TestController.java
@@ -1,11 +1,19 @@
 package com.side.billim;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+	@Operation(summary = "String test", description = "문구 테스트")
+	@ApiResponses({
+			@ApiResponse(responseCode = "200", description = "통과"),
+			@ApiResponse(responseCode = "400", description = "실패")
+	})
 	@GetMapping("/api/hello")
 	public String Hello(){
 		return "Hello World!";
