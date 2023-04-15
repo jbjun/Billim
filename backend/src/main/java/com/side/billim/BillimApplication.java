022de5fe117e0ef387310f14f116a82c33a2ab53
ssooya <ssooya90@naver.com>
2023-04-15 10:20:48 +0900

    feat @EnableJpaAuditing 추가

--- backend/src/main/java/com/side/billim/BillimApplication.java
+++ backend/src/main/java/com/side/billim/BillimApplication.java
@@ -2,7 +2,9 @@ package com.side.billim;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class BillimApplication {
     public static void main(String[] args) {
