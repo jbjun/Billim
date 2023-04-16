3fc0e8f09a3f19aa7f17cb43005dafd0e2fed453
ssooya <ssooya90@naver.com>
2023-04-16 18:22:00 +0900

    fix chat 수정

--- backend/src/main/java/com/side/billim/chat/web/ChatController.java
+++ backend/src/main/java/com/side/billim/chat/web/ChatController.java
@@ -7,30 +7,18 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
 @Log4j2
 @RequiredArgsConstructor
-@Controller
+@RestController
 @RequestMapping("/chat")
 public class ChatController {
 
 	private final ChatService chatService;
-
-	@GetMapping("/rooms")
-	public String rooms(Model model) {
-
-		return "/chat/rooms";
-
-	}
 
 	@Operation(summary = "채팅방 목록", description = "채팅방 목록")
 	@ApiResponses({
@@ -50,17 +38,6 @@ public class ChatController {
 		return "redirect:/chat/room";
 	}
 
-	//채팅방 조회
-	@GetMapping("/room")
-	public String getRoom(String roomId, Model model){
-
-		log.info("# get Chat Room, roomID : " + roomId);
-
-		return "/chat/room";
-
-
-//		model.addAttribute("room", repository.findRoomById(roomId));
-	}
 
 
 
