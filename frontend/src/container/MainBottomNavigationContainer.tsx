8a8d844a7e99015006e13dbcb3fd36a12228e852
nabisorry <cdb131450@gmail.com>
2023-05-01 00:24:24 +0900

    bottom navigation bar icon 교체

--- frontend/src/container/MainBottomNavigationContainer.tsx
+++ frontend/src/container/MainBottomNavigationContainer.tsx
@@ -7,21 +7,22 @@ import {
   SvgIcon,
 } from "@mui/material";
 import { useLocation, useNavigate } from "react-router";
-import PersonIcon from "@mui/icons-material/Person";
-import ListAltIcon from "@mui/icons-material/ListAlt";
-import QuestionAnswerIcon from "@mui/icons-material/QuestionAnswer";
 
 // 내부모듈
-import { ReactComponent as HomeIcon } from "@assets/icons/Home_outlined.svg";
+import { ReactComponent as OHomeIcon } from "@assets/icons/Home_outlined.svg";
+import { ReactComponent as FHomeIcon } from "@assets/icons/Home_filled.svg";
+import { ReactComponent as OListIcon } from "@assets/icons/List_outlined.svg";
+import { ReactComponent as FListIcon } from "@assets/icons/List_fiiled.svg";
+import { ReactComponent as OChatIcon } from "@assets/icons/Chat_outlined.svg";
+import { ReactComponent as FChatIcon } from "@assets/icons/Chat_filled.svg";
+import { ReactComponent as OProfileIcon } from "@assets/icons/Profile_outlined.svg";
+import { ReactComponent as FProfileIcon } from "@assets/icons/Profile_filled.svg";
 import { CHAT_LIST_PATH } from "routes";
-
-// import { ReactComponent as ListIcon } from "../../assets/icons/List_outlined.svg";
-// import { ReactComponent as ChatIcon } from "../../assets/icons/Chat_outlined.svg";
-// import { ReactComponent as ProfileIcon } from "../../assets/icons/Profile_outlined.svg";
 
 const LabelBottomNavigation = () => {
   const navigate = useNavigate();
   const location = useLocation();
+  const { pathname } = location;
   const [value, setValue] = useState(location.pathname);
   const handleChange = (event: React.SyntheticEvent, path: string) => {
     if (location.pathname === path) return;
@@ -38,22 +39,46 @@ const LabelBottomNavigation = () => {
         <BottomNavigationAction
           label="홈"
           value="/home"
-          icon={<SvgIcon component={HomeIcon} />}
+          icon={
+            pathname === `/home` ? (
+              <SvgIcon component={FHomeIcon} />
+            ) : (
+              <SvgIcon component={OHomeIcon} />
+            )
+          }
         />
         <BottomNavigationAction
           label="대여 내역"
           value="/rentalList"
-          icon={<SvgIcon component={ListAltIcon} />}
+          icon={
+            pathname === `/rentalList` ? (
+              <SvgIcon component={FListIcon} />
+            ) : (
+              <SvgIcon component={OListIcon} />
+            )
+          }
         />
         <BottomNavigationAction
           label="채팅"
           value={`/${CHAT_LIST_PATH}`}
-          icon={<SvgIcon component={QuestionAnswerIcon} />}
+          icon={
+            pathname === `/${CHAT_LIST_PATH}` ? (
+              <SvgIcon component={FChatIcon} />
+            ) : (
+              <SvgIcon component={OChatIcon} />
+            )
+          }
         />
         <BottomNavigationAction
           label="내 정보"
           value="/my"
-          icon={<SvgIcon component={PersonIcon} />}
+          icon={
+            pathname === "/my" ? (
+              <SvgIcon component={FProfileIcon} />
+            ) : (
+              <SvgIcon component={OProfileIcon} />
+            )
+          }
         />
       </BottomNavigation>
     </Paper>
