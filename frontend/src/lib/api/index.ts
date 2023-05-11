94c7a5c6162ff37466ab558417d23354fb6f6bab
byeongjun <wkdqud@dahanit.co.kr>
2023-05-11 14:15:15 +0900

    master fix BASE_API_PATH modified

--- frontend/src/lib/api/index.ts
+++ frontend/src/lib/api/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getCookie } from "./cookie";
 export const BASE_URL = "http://44.203.121.190:8080";
 // proxy 설정 시 타겟으로 지정할 path vite.config.ts
-export const BASE_API_PATH = "/api";
+export const BASE_API_PATH = "";
 export const BASE_SOKET_IO_PATH = "/socket.io";
 export const setHeaderAuthorization = (sessionKey: string) => {
   if (!sessionKey) return;
